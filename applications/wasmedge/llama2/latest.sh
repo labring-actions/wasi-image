26ceb6d93f10fb2fe6385166cf44f7eb09a3ba6b
cuisongliu <cuisongliu@qq.com>
2023-11-15 22:45:55 +0800

    Update latest.sh

--- applications/wasmedge/llama2/latest.sh
+++ applications/wasmedge/llama2/latest.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 set -ex
-curl -LO https://huggingface.co/wasmedge/llama2/resolve/main/llama-2-7b-chat-q5_k_m.gguf
+#curl -LO https://huggingface.co/wasmedge/llama2/resolve/main/llama-2-7b-chat-q5_k_m.gguf
+curl -LO https://huggingface.co/second-state/Dolphin-2.2-Yi-34B-GGUF/resolve/main/dolphin-2.2-yi-34b-ggml-model-q4_0.gguf
 ls -lh
